c44c1a6d3c7324ff548b89952ada04b06b99d54d
asd <asd>
2019-04-10 04:37:21 +0000

    think i got the deserialization working

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ use std::collections::HashMap;
 use std::env;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+extern crate serde_json;
+
+
+
 //holds data for instructor and students
 pub struct ClassIssue {
 	class_repo_address: String,
@@ -25,23 +29,39 @@ impl ClassIssue {
 
     pub fn write_issue( &self)-> Result<(), Box<std::error::Error>> {        
         // This will POST a body of `{"lang":"rust","body":"json"}`
-        let mut map = HashMap::new();
-        map.insert("lang", "rust");
-        map.insert("body", "json");
+        // let mut map = HashMap::new();
+        // map.insert("name", "asdasd");
+        // map.insert("private", "false");
+       
+        let mut url_str = String::new();
+        url_str.push_str(&format!("{}/issues/",self.class_repo_address));
 
-        let client = reqwest::Client::new();
+        let url = reqwest::Url::parse(&url_str).expect("invalid issue writing url");
         
-        let url = reqwest::Url::parse(&self.class_repo_address).expect("invalid issue writing url");
+        let client = reqwest::Client::new();
         let res = client.post(url)
             .basic_auth(&self.username,Some(&self.password))
-            .json(&map)
+            //.json(&map)
             .send()?;
 
         dbg!(res);
         Ok(())
     }
 
-    pub fn get_issue( &self, issue_num:i32){
+    pub fn get_issue( &self, issue_num:i32) -> Result<String, reqwest::Error>{
+        let mut url_str = String::new();
+        url_str.push_str(&format!("{}/issues/{}",self.class_repo_address,issue_num));
+        dbg!(&  url_str);
+        let url = reqwest::Url::parse(&url_str).expect("invalid issue writing url");
+        
+        let client = reqwest::Client::new();
+        let mut res = client.get(url)
+            .send()?;
+        let body = res.text().expect("error parsing");
+
+        let deser:serde_json::Value = serde_json::from_str(&body).expect("error parsinge");
+        dbg!(deser);
+        Ok(body)
     }
     pub fn close_issue( &self, issue_num:i32){
     }
@@ -60,9 +80,8 @@ mod tests {
     fn add_issue() {
         let username = "hortinstein";
         let password = env::var("GITHUB_PASSWORD").expect("set the GITHUB_PASSWORD env");
-        let class_repo_address = "https://www.github.com/replicatedu/issue_database";
+        let class_repo_address = "https://api.github.com/repos/replicatedu/issue_database";
         let issue_db = ClassIssue::new(class_repo_address.to_string(),username.to_string(),password.to_string());
-        println!("asdasd");
-        issue_db.write_issue();
+        dbg!(issue_db.get_issue(1));
     }
 }
